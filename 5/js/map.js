68996e320bfc55979f0b3fff4235a6ddd370eaba
Keks <keks@htmlacademy.ru>
2022-12-28 11:48:11 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/map.js
@@ -0,0 +1,6 @@
+const mapImage = document.querySelector('.map__map-image');
+const mapGoogle = document.querySelector('.map__google');
+
+mapImage.outerHTML = "";
+
+mapGoogle.classList.remove('map__google--nojs');
