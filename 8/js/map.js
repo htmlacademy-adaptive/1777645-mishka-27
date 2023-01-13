9dbe642d2781f36e88687f6f3a5523203ca095f1
Keks <keks@htmlacademy.ru>
2023-01-13 16:00:00 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/map.js
@@ -0,0 +1,1 @@
+const mapImageTag=document.querySelector(".contacts__map.map > picture"),mapGoogle=document.querySelector(".map__google");mapImageTag.remove(),mapGoogle.classList.remove("map__google--nojs");
